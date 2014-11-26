baf9af39a09f75af3d2849fe3726cba81046ee2c
Griff Hall <griffhall1@gmail.com>
2014-11-26 09:57:15 -0600

    Initial Commit

---
+++ to_do_list/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  to_do_list
+//
+//  Created by Griff Hall on 11/26/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
